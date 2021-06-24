16b4847cb434f2f7b6f0e7edc7c883056b6d53c7
rez <rez182@gmail.com>
2021-06-24 04:01:22 -0400

    added invert capability

--- img2txt.py
+++ img2txt.py
@@ -8,24 +8,29 @@ import numpy as np
 
 # minimum arguments required to convert an image
 if(len(sys.argv) < 3):
-	print("Need arguments:\n1) image file\n2) threshold\nAnd optional binary flag and/or pretty flag")
-	print("eg: python3 image1.jpg 200 [ -b -p (optional) ]")
+	print("Need arguments:\n1) image file\n2) threshold\nAnd optional binary / pretty / invert flags")
+	print("eg: python3 image1.jpg 200 [ -b -p -i (optional) ]")
 	exit()
 
 binary = False
 pretty = False
+invert = False
 threshold = int(sys.argv[2])
 img = Image.open(sys.argv[1]).convert('L')	# convert image to greyscale
 col, row = img.size
 
-if(len(sys.argv) == 5):
+if(len(sys.argv) == 6):
 	binary = True
 	pretty = True
+	invert = True
 else:
-	if(sys.argv[3] == "-b"):
-		binary = True
-	if(sys.argv[3] == "-p"):
-		pretty = True
+	for i in range(3, len(sys.argv)):
+		if(sys.argv[i] == "-b"):
+			binary = True
+		if(sys.argv[i] == "-p"):
+			pretty = True
+		if(sys.argv[i] == "-i"):
+			invert = True
 
 # convert PIL.image to 2D np.array or greyscale values
 x = np.asarray(img.getdata(), dtype=np.int).reshape((img.size[1], img.size[0]))
@@ -44,27 +49,33 @@ st = ""
 
 for i in range(row):
 	for j in range(col):
-		if x[i][j] <= threshold:
-			if not binary:
-				if minVal > x[i][j]:
-					minVal = x[i][j]
-				if maxVal < x[i][j]:
-					maxVal = x[i][j]
-				st += str(x[i][j]) +" "
 
-			else:
-				if x[i][j] > 0:
-					st += "1 "
-				else:
-					if pretty:
-						st += "· "
-					else:
-						st += "0 "
+		cur_pixel = None
+
+		if invert and x[i][j] >= threshold:
+			cur_pixel = x[i][j]
+
+		if not invert and x[i][j] <= threshold:
+			cur_pixel = x[i][j]
+
+		if not binary and cur_pixel is not None:
+
+			if minVal > cur_pixel:
+				minVal = cur_pixel
+
+			if maxVal < cur_pixel:
+				maxVal = cur_pixel
+
+			st += str(cur_pixel) +" "
+
 		else:
-			if pretty:
-				st += ". "
+			if cur_pixel is not None and cur_pixel > 0:
+				st += "1 "
 			else:
-				st += "0 "
+				if pretty:
+					st += "· "
+				else:
+					st += "0 "
 
 	st += "\n"
 
